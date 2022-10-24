f9a8704a05e590056387298056a8fea51fc886cd
Dan Molik <dan@danmolik.com>
2022-10-24 11:01:58 -0400

    updates for no access key

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,13 @@
 set -e
 
 if [ -z "$AWS_S3_BUCKET" ]; then
-  echo "AWS_S3_BUCKET is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
-
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
+	echo "AWS_S3_BUCKET is not set. Quitting."
+	exit 1
 fi
 
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
+	AWS_REGION="us-east-1"
 fi
 
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
